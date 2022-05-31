d287a07f8c52598caeefd71170930c0da8a66798
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-31 19:21:47 -0300

    exercicio 04 feito

--- 04/scripts.js
+++ 04/scripts.js
@@ -1,1 +1,17 @@
+function atendimento(lista, operacao, nome) {
+    if (operacao === "atender") {
+        lista.shift();
+    }
+
+    if (operacao === "agendar") {
+        lista.push(nome);
+    }
+    console.log(lista.join(', '));
+}
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+
+atendimento(pacientes, 'atender');
+atendimento(pacientes, 'atender');
+atendimento(pacientes, 'agendar', 'Danilo');
+atendimento(pacientes, 'atender');
+
